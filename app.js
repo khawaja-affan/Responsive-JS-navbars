5ad93089566a51977f4a6fabbf70ecc25e0a09c0
KHAWAJA-AFFAN-KHURRAM <khawajaaffan0321@gmail.com>
2025-09-19 00:05:01 +0500

    init

---
+++ app.js
@@ -0,0 +1,9 @@
+function menu() {
+        let all = document.getElementById("all");
+
+        if (all.style.display === "block") {
+            all.style.display = "none";
+        } else {
+            all.style.display = "block";
+        }
+    }
